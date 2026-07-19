9b1b278aedd79f2e7856912d8469e4a19d000bb9
hisyamsz <hisyam.s901@gmail.com>
2026-07-19 21:09:23 +0700

    chore: initialize dependencies, eslint, and prettier for Astro rewrite

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['*.astro'],
+    languageOptions: {
+      parser: await import('astro-eslint-parser'),
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  },
+  {
+    ignores: ['dist/', '.astro/', 'node_modules/']
+  }
+];
