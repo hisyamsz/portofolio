8fa255401fa18d0d856652d602e4f26569889ab9
hisyamsz <hisyam.s901@gmail.com>
2026-07-19 23:49:57 +0700

    chore: update site URL and add aria-label to back-to-top button

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hisyamsz.github.io',
+  site: 'https://hisyamsantoso.my.id',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
